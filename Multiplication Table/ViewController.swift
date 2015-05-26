ded60ff7c9e1c9745c4949ffbbce94c55f024d00
VLT Labs <vltlabs@VLTs-MacBook-Pro.local>
2015-05-26 21:32:36 +0800

    Initial Commit

---
+++ Multiplication Table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiplication Table
+//
+//  Created by VLT Labs on 5/26/15.
+//  Copyright (c) 2015 Jay Ang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
